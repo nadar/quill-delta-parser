de9c81d707f0793f5625b34c3ac099fabdc6bca3
Basil Suter <git@nadar.io>
2018-09-24 21:57:34 +0200

    add tests

---
+++ tests/LexerTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace nadar\quill\tests;
+
+use nadar\quill\Lexer;
+use PHPUnit\Framework\TestCase;
+
+
+class LexerTest extends TestCase
+{
+    public function testIsJson()
+    {
+        $this->assertTrue(Lexer::isJson('{}'));
+        $this->assertFalse(Lexer::isJson([]));
+    }
+}
---
+++ tests/LineTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace nadar\quill\tests;
+
+use nadar\quill\Lexer;
+use nadar\quill\Line;
+use PHPUnit\Framework\TestCase;
+
+
+class LineTest extends TestCase
+{
+    public function testsNextLine()
+    {
+        $lexer = new Lexer(['insert' => 'foo']);
+
+        $line = new Line(0, 'input', [], $lexer, false);
+        $this->assertFalse($line->next());
+        $this->assertFalse($line->next(function(Line $line) {
+            return true;
+        }));
+        $this->assertFalse($line->next(function(Line $line) {
+            return false;
+        }));
+
+    }
+}
