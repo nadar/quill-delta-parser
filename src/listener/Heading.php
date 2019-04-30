c70e75764108d0878251374d2f8d8964302bd9a1
Lode Claassen <lode@alsvanzelf.nl>
2019-04-30 14:05:36 +0200

    CS

--- src/listener/Heading.php
+++ src/listener/Heading.php
@@ -16,6 +16,7 @@ class Heading extends BlockListener
 {
     /**
      * @var array Supported header levels.
+     * @since 1.2.0
      */
     public $levels = [1, 2, 3, 4, 5, 6];
 
@@ -40,7 +41,7 @@ class Heading extends BlockListener
     public function render(\nadar\quill\Lexer $lexer)
     {
         foreach ($this->picks() as $pick) {
-            if (!in_array((int)$pick->heading, $this->levels)) {
+            if (!in_array($pick->heading, $this->levels)) {
                 // prevent html injection in case the attribute is user input
                 throw new Exception('An unknown heading level "'.$pick->heading.'" has been detected.');
             }
