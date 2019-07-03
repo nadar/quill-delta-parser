fe7df9d17d8649fa11e3239a28907eaad12f91ca
Jasper Wissels <jwissels@gmail.com>
2019-07-03 10:08:56 +0200

    changed the test to reflect formatting before headers now work

--- tests/BoldContentTest.php
+++ tests/BoldContentTest.php
@@ -4,18 +4,11 @@ namespace nadar\quill\tests;
 class BoldContentTest extends DeltaTestCase
 {
     public $json = <<<'JSON'
-{"ops": [
-  {"insert": "Heading 1","attributes": {"bold": true}},
-  {"insert": "\n","attributes": {"header": 1}},
-  {"insert": "list"},
-  {"insert": "\n","attributes": {"list": "bullet"}},
-  {"insert": "list"},{"attributes": {"list": "bullet"},"insert": "\n"},
-  {"insert": "Heading 2"},{"attributes": {"header": 2},"insert": "\n"}
-]}
+{"ops":[{"attributes":{"bold":true},"insert":"Formatted"},{"insert":"\n"},{"attributes":{"bold":true},"insert":"Heading 1"},{"attributes":{"header":1},"insert":"\n"},{"insert":"list"},{"attributes":{"list":"bullet"},"insert":"\n"},{"insert":"list"},{"attributes":{"list":"bullet"},"insert":"\n"},{"insert":"Heading 2"},{"attributes":{"header":2},"insert":"\n"}]}
 
 JSON;
 
     public $html = <<<'EOT'
-<h1><strong>Heading 1</strong></h1><ul><li>list</li><li>list</li></ul><h2>Heading 2</h2>
+<p><strong>Formatted</strong></p><h1><strong>Heading 1</strong></h1><ul><li>list</li><li>list</li></ul><h2>Heading 2</h2>
 EOT;
 }
