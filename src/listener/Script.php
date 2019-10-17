5bab864f20c0111afe16f3d59fba1202207fc31b
Faugère Gaëtan <gaetan@fauge.re>
2019-10-17 15:13:44 +0200

    Add inline element to handle script attribute. (#27)
    * Add inline element to handle script attribute.
    
    * Fix issues related to script listener

---
+++ src/listener/Script.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace nadar\quill\listener;
+
+use Exception;
+use nadar\quill\InlineListener;
+use nadar\quill\Line;
+
+/**
+ * Renders script attribute which will generate sup/sub tags.
+ * 
+ * @author Gaëtan Faugère <gaetan@fauge.re>
+ * @since 2.2.0
+ */
+class Script extends InlineListener
+{
+
+    public $scriptTags = ['super', 'sub'];
+
+    /**
+     * {@inheritDoc}
+     */
+    public function process(Line $line)
+    {
+        if (($script = $line->getAttribute('script'))) {
+            $this->updateInput($line, $this->applyTemplate($script, $line));
+        }
+    }
+
+    /**
+     * Wrap in sup/sub tag.
+     *
+     * @param string $script
+     * @param Line $line
+     * @throws Exception for unknown script tag
+     * @return string
+     */
+    public function applyTemplate($script, Line $line)
+    {
+        if (!in_array($script, $this->scriptTags)) {
+            // prevent html injection and wrong behaviors
+            throw new Exception('An unknown script tag "' . $script . '" has been detected.');
+        }
+        if ($script === 'super') {
+          $script = 'sup';
+        }
+
+        return '<'.$script.'>'. $line->input . '</'.$script.'>';
+    }
+}
