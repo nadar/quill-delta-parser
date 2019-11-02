8e5cdb55b31d2b5452e1fa84c9feb52670c34edd
Faugère Gaëtan <gaetan@fauge.re>
2019-11-02 11:24:22 +0100

    Update to wrapElement method

--- src/listener/Align.php
+++ src/listener/Align.php
@@ -45,22 +45,8 @@ class Align extends BlockListener
                 throw new Exception('An unknown alignment "' . $pick->alignment . '" has been detected.');
             }
 
-            $first = $this->getFirstLine($pick);
-
-            // while from first to the pick line and store content in buffer
-            $buffer = null;
-            $first->while(function (&$index, Line $line) use (&$buffer, $pick, $first) {
-                $index++;
-                $buffer .= $line->getInput();
-                $line->setDone();
-                // if the index of the picked lines is reached or the first element is the picked index.
-                if ($index == $pick->line->getIndex() || $first->getIndex() == $pick->line->getIndex()) {
-                    return false;
-                }
-            });
-
-            $pick->line->output = '<p style="text-align: ' . $pick->alignment . ';">' . $buffer . '</p>';
-            $pick->line->setDone();
         }
+
+        $this->wrapElement('<p style="text-align: {alignment};">{__buffer__}</p>', ['alignment']);
     }
 }
