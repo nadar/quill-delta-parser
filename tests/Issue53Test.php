31b168729992520a48027defb5161e21affc353e
Basil <git@nadar.io>
2021-10-27 10:53:32 +0200

    Create Issue53Test.php

---
+++ tests/Issue53Test.php
@@ -0,0 +1,24 @@
+<?php
+namespace nadar\quill\tests;
+
+class Issue53Test extends DeltaTestCase
+{
+    public $json = <<<'JSON'
+{
+    "attributes": {
+      "list": {
+        "depth": 0,
+        "type": "bullet"
+      }
+    },
+    "insert": "\n"
+  },
+  {
+    "insert": "Bullet point content"
+  },
+JSON;
+
+    public $html = <<<'EOT'
+    <ul><li>Bullet point content</li></ul>
+EOT;
+}
