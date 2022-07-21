e599a29c322c3b9cde998c851a8c8e0470ee7b0d
Basil Suter <git@nadar.io>
2022-07-21 11:37:24 +0000

    phpstan

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,16 @@
+<?php
+
+$finder = PhpCsFixer\Finder::create()
+    ->exclude('tests')
+    ->in(__DIR__)
+;
+
+$config = new PhpCsFixer\Config();
+return $config->setRules([
+        '@PSR12' => true,
+        'array_syntax' => ['syntax' => 'short'],
+        'ordered_imports' => ['sort_algorithm' => 'alpha'],
+        'no_unused_imports' => true,
+    ])
+    ->setFinder($finder)
+;
--- output.php
+++ output.php
@@ -3,8 +3,8 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-use nadar\quill\Lexer;
 use nadar\quill\Debug;
+use nadar\quill\Lexer;
 
 require 'vendor/autoload.php';
 
