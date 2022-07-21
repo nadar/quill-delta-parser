d5cb61f337b09f83db9de7727f89ae288ac4e0cd
Lode Claassen <lode@alsvanzelf.nl>
2022-07-21 22:38:25 +0200

    allow to define json fixtures as php arrays for better readability
    incl. 2 example test classes converted

--- tests/LinkWrapperOverrideTest.php
+++ tests/LinkWrapperOverrideTest.php
@@ -6,7 +6,17 @@ use nadar\quill\listener\Link;
 
 class LinkWrapperOverrideTest extends DeltaTestCase
 {
-    public $json = '[{"attributes":{"link":"https://luya.io"},"insert":"luya.io"},{"insert":" test\n\nFooter\n"}]';
+    public $json = [
+        [
+            'attributes' => [
+                'link' => 'https://luya.io',
+            ],
+            'insert' => 'luya.io',
+        ],
+        [
+            'insert' => ' test'.PHP_EOL.PHP_EOL.'Footer'.PHP_EOL,
+        ],
+    ];
 
     public $html = '<p><a href="https://luya.io">luya.io</a> test</p><p><br></p><p>Footer</p>';
 
